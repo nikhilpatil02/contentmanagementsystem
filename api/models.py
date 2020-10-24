e3b63c64265ce56b1cf086a6f85dcd056fc8bff2
Nikhil Patil <nikhilpatil@significai.com>
2020-10-25 00:04:00 +0530

    added two models/tables

--- api/models.py
+++ api/models.py
@@ -13,6 +13,21 @@ class Users(models.Model):
     state = models.CharField(max_length=100, null=True)
     country = models.CharField(max_length=100, null=True)
     type_of_user = models.CharField(max_length=10, default="Author")
+
+class Content(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.CharField(max_length=300)
+    summary = models.CharField(max_length=60)
+    #document = models.BinaryField()
+    user = models.ForeignKey(Users,on_delete = models.CASCADE)
+
+class Categories(models.Model):
+    content = models.ForeignKey(Content, on_delete = models.CASCADE)
+    category_name = models.CharField(max_length=30)
+    
+
+
+
     
     
     
